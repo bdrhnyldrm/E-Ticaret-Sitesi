2c426f72f7b506a924bac291554b69a471b34179
Bedirhan Yıldırım <100311564+bdrhnyldrm@users.noreply.github.com>
2025-08-13 16:48:43 +0300

    Delete Commit-2 directory

--- Commit-2/ecommerce/src/main/java/com/ecommerce/dto/CategorySummary.java
+++
@@ -1,9 +0,0 @@
-package com.ecommerce.dto;
-
-import com.ecommerce.model.Category;
-
-public record CategorySummary(Long id, String name) {
-    public static CategorySummary from(Category c) {
-        return new CategorySummary(c.getId(), c.getName());
-    }
-}
